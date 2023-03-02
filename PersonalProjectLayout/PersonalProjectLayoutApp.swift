2f1fdcba1aa29371dfd32c22eb48492910d5edfb
Benjamin Lui <benjaminlui00@gmail.com>
2023-03-02 14:42:37 -0800

    Initial Commit

---
+++ PersonalProjectLayout/PersonalProjectLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalProjectLayoutApp.swift
+//  PersonalProjectLayout
+//
+//  Created by Benjamin Lui on 2023-03-02.
+//
+
+import SwiftUI
+
+@main
+struct PersonalProjectLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
