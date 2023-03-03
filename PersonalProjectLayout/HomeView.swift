4381b3d1f0caa7f074f195f3d021c14b7c85ff69
Benjamin Lui <benjaminlui00@gmail.com>
2023-03-02 17:32:56 -0800

    first commit

---
+++ PersonalProjectLayout/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  PersonalProjectLayout
+//
+//  Created by Benjamin Lui on 2023-03-02.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ PersonalProjectLayout/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  PersonalProjectLayout
+//
+//  Created by Benjamin Lui on 2023-03-02.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
